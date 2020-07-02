b60fe02b905859ce8718382cfd70644833c1dd04
icstreispe <icstreispe@yahoo.com>
2020-07-02 11:16:47 +0300

    Db - tip strada catalog + ajustari scripturi

--- db/src/main/resources/db/migration/V1_0_6__tip_structura.sql
+++ db/src/main/resources/db/migration/V1_0_6__tip_structura.sql
@@ -2,6 +2,7 @@ create table tip_structura (
     id integer primary key,
     name national character varying (100),
     start_date date default now(),
+    update_date date default now(),
     end_date date
 );
 
