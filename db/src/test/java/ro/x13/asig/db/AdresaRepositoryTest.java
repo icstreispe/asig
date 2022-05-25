20c166814144bea38cc34227b34ebce9d08f4cdf
icstreispe <icstreispe@yahoo.com>
2022-05-25 23:03:29 +0300

    upgrade postresql + adding jpa tests

---
+++ db/src/test/java/ro/x13/asig/db/AdresaRepositoryTest.java
@@ -0,0 +1,43 @@
+package ro.x13.asig.db;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ro.x13.asig.db.dao.AdresaRepository;
+import ro.x13.asig.db.dao.domain.Adresa;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class AdresaRepositoryTest {
+
+    @Autowired
+    AdresaRepository adresaRepository;
+
+    @BeforeEach
+    void init (){
+
+    }
+
+    @Test
+    void whenSavedThenFound(){
+        Adresa adresa = Adresa.builder().apartament("ap1").build();
+        Adresa a2 = adresaRepository.save(adresa);
+        assertNotNull (a2);
+
+        List<Adresa> l = adresaRepository.findAllByOrderById();
+        assertEquals(l.size(), 1);
+        assertEquals(l.get(0), a2);
+        //TODO test other fields
+    }
+
+    @Test
+    void whenNotSavedThenNotFound (){
+        //TODO
+    }
+
+}
---
+++ db/src/test/java/ro/x13/asig/db/TestApp.java
@@ -0,0 +1,8 @@
+package ro.x13.asig.db;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//only when
+@SpringBootApplication
+public class TestApp {
+}
